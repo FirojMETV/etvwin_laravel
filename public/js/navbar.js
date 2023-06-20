3a8bc61c807b243c29eea4bde16fe31f1bafb61b
Firoz <firoz.m@etv.co.in>
2023-06-20 17:09:47 +0530

    navbar with side drawer image is not setup in background-image for side drawer

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,11 +1,22 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "450px";
+  document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("mySidenav").style.width = "270px";
+  document.getElementById("main").style.marginLeft = "350px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("mySidenav").style.display = "none";
+
+}
+
+// search icon 
+function openSearch(){
+  document.getElementById("myOverlay").style.display="block";
+}
+function closeSearch(){
+  document.getElementById('myOverlay').style.display='none';
 }
--- resources/views/Header/header.blade.php
+++ resources/views/Header/header.blade.php
@@ -1,21 +1,25 @@
 <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
 
-<div class="navbar navbar-inverse ">
-    <div class=" d-flex container-fluid justify-content-center" style="">
+<div class="navbar-inverse " style="justify-content:space-between">
+    <div class="container-fluid" style="display:inline-flex;">
+        {{-- menu icon --}}
+        <div class="visible-xs" style="margin-top:10px">
+            <span style="font-size:30px;cursor:pointer ;
+             color:aliceblue" onclick="openNav()">
+                &#9776;
+            </span>
+        </div>
         {{-- left image --}}
-    <div class="hidden-lg"> <span style="font-size:30px;cursor:pointer ; color:aliceblue" onclick="openNav()">&#9776; </span>
-    </div>
-        <div class="header">
-
-            <a class="navbar-brand" href="#">
-                <img src="{{ asset('image/et.jpg') }}" height="50px" width="50px" alt=""
-                    class="logo-img sm:pd-6">
+        <div class="header mr-auto" style=" justify-content:space-around">
+            <a class="navbar-brand ml-5" href="#">
+                <img src="{{ asset('image/et.jpg') }}" height="40px" width="50px" alt=""
+                    class="logo-img sm:pd-6 ">
             </a>
         </div>
         {{-- center text  --}}
-        <ul class=" nav navbar-nav   hidden-xs mr-auto">
+        <ul class=" nav navbar-nav hidden-xs " style="margin: 10px 10px 5px 0px">
             <li class="active">
-                <a class=" " href="{{ url('/home') }}">HOME</a>
+                <a class="nav-link " href="{{ url('/home') }}">HOME</a>
             </li>
             <li class="nav-item ">
                 <a class="nav-link" href="{{ url('/tv-shows') }}">TV SHOWS</a>
@@ -36,58 +40,80 @@
                 <a class="nav-link" href="{{ url('/health') }}">HEALTH</a>
             </li>
         </ul>
-        {{-- right content --}}
-        <div class="d-flex  navbar-right ">
-            <ul class="nav navbar-nav flex-direction-sm-row ">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('subscribe') }}">
-                        <img src="{{ asset('image/subscribe.png') }}" height="50px" width="120px" alt=""
-                            class="subscription-img">
-                    </a>
-                </li>
-                <li class="nav-item ">
-                    <a class="nav-link " href="{{ url('/search') }}">
-                        <img src="{{ asset('image/search.png') }}" height="35px" width="35px" alt=""
-                            class="search-img " />
-                    </a>
-                </li>
-                <li class="dropdown hidden-xs">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" href="{{ url('/profile') }}">
-                        <img src="{{ asset('image/profile.png') }}" height="45px" width="45px" alt=""
-                            class="profile-img  color-#00021d " />
-                        <ul class="dropdown-menu "
-                            style="background: #191D4F ; min-width:280px ; padding:10px 50px; font-size:15px;  ">
-                            <li style="padding-bottom:22px"><a href="#" style="color: #e7b851 ; ">SIGN IN</a>
-                            </li>
-                            <li><a href="#" style="color:#e7b851">SIGN OUT</a></li>
 
-                        </ul>
-                    </a>
-                </li>
+
+        {{-- right content --}}
+        <div class="" style="display:inline-flex; ">
+            <div style="display:inline; margin:10px 5px 10px 10px ">
+                <a class="nav-link" href="{{ url('subscribe') }}">
+                    <img src="{{ asset('image/subscribe.png') }}" height="50px" width="120px" alt=""
+                        class="subscription-img">
+            </div>
+            <div style="display: inline; margin:20px 5px 10px 10px">
+                <a class="nav-link  " href="javascript:void(0);">
+                    <img src="{{ asset('image/search.png') }}" height="35px" width="35px" alt=""
+                        class="search-img " onclick="openSearch()" />
+                </a>
+            </div>
+            <div class="dropdown hidden-xs" style="display:inline;">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" href="{{ url('/profile') }}">
+                    <img src="{{ asset('image/profile.png') }}" height="45px" width="45px" alt=""
+                        class="profile-img   " />
+                </a>
+                <ul class="dropdown-menu "
+                    style="background: #191D4F !important ;
+                             min-width:280px ; padding:10px 50px; font-size:15px;  ">
+                    <li style="padding-bottom:22px">
+                        <a href="#" style="color: #e7b851 ; background:#191D4F !important ">SIGN IN</a>
+                    </li>
+                    <li>
+                        <a href="#" style="color:#e7b851 ; background:#191D4F !important;">SIGN OUT</a>
+                    </li>
+                </ul>
+            </div>
 
             </ul>
-
-        </div>
-        <div id="mySidenav" class="sidenav hidden-xs">
-            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-          <div >
-            <a href="#">Home</a>
-            <a href="#">TV SHOWS</a>
-            <a href="#">MOVIES</a>
-            <a href="#">LIVE TV</a>
-            <a href="#">NEWS</a>
-            <a href="#">FOOD </a>
-            <a href="#">HEALTH</a>
-          </div>
-        </div>
-
-        <!-- Use any element to open the sidenav -->
-
-
-        <!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
-        <div id="main">
-            ...
         </div>
     </div>
+</div>
 
-    <script src="{{ asset('js/navbar.js') }}"></script>
+<div id="mySidenav" class="sidenav " style="display:none;">
+    <div class="signin" style="background: linear-gradient(#598a59,#91914a) ; ">
+        hey guest user !!
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+        <div class="d-inline" style="margin-bottom:10px;">
+            <button href="#" type="button" class="col-sm-1 col-xs-6 btn form-button blue-button">SIGN IN</button>
+            <button href="#" type="button"  class="col-sm-1 col-xs-6 btn form-button blue-button">
+                SIGN UP
+            </button>
+        </div>
+    </div>
+    <hr class="rounded">
+    <div>
+        <a href="#">Home</a>
+        <hr class="rounded">
+        <a href="#">TV SHOWS</a>
+        <hr class="rounded">
+        <a href="#">MOVIES</a>
+        <hr class="rounded">
+        <a href="#">LIVE TV</a>
+        <hr class="rounded">
+        <a href="#">NEWS</a>
+        <hr class="rounded">
+        <a href="#">FOOD</a>
+        <hr class="rounded">
+        <a href="#">HEALTH</a>
+    </div>
+</div>
+<div id="myOverlay" class="overlay">
+    <span class="closebtn" onclick="closeSearch()" title="Close Overlay">×</span>
+    <div class="overlay-content">
+        <form action="/action_page.php">
+            <input type="text" placeholder="Search.." name="search">
+
+        </form>
+    </div>
+</div>
+
+<script src="{{ asset('js/navbar.js') }}"></script>
