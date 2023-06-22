e0e757e877211d224204d8552b6fc8510b598f23
Firoz <firoz.m@etv.co.in>
2023-06-22 12:02:00 +0530

    sidebar is completed and navbar left some issue or login is working

--- resources/views/Layout.blade.php
+++ resources/views/Layout.blade.php
@@ -13,8 +13,11 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
-
+<style>
+    body{
+        background: #00021C;
+    }
+</style>
 </head>
 
 <body>
