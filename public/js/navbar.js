39c8764dcc86883328fcac26fa3e186f70abc9d2
Firoz <firoz.m@etv.co.in>
2023-06-23 17:26:16 +0530

    validation for confirm password is panding

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,8 +2,8 @@
 function openNav() {
   document.getElementById("mySidenav").style.display = "block";
   document.getElementById("mySidenav").style.width = "270px";
- 
-  document.getElementById('mydownload').style.display='block';
+
+  document.getElementById('mydownload').style.display = 'block';
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
@@ -11,14 +11,32 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 
   document.getElementById("mySidenav").style.display = "none";
-  document.getElementById('mydownload').style.display='none';
-}
- 
-// search icon 
-function openSearch(){
-  document.getElementById("myOverlay").style.display="block";
-}
-function closeSearch(){
-  document.getElementById('myOverlay').style.display='none';
+  document.getElementById('mydownload').style.display = 'none';
 }
 
+// search icon 
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+function closeSearch() {
+  document.getElementById('myOverlay').style.display = 'none';
+}
+
+function checkpassword() {
+  const password = document.getElementById('password').value;
+  const confirmpassword = document.getElementById('confirm-password').value;
+  console.log(password, confirmpassword)
+  var messsage = document.getElementById('message');
+  if (password.length != 0) {
+    if (password == confirmpassword) {
+      messsage.textContent = 'password match';
+      messsage.style.color="#6854"
+    } else {
+      messsage.textContent = "password don't match";
+      messsage.style.color="#ff55"
+    }
+  }else{
+    alert('password can not be empty');
+    messsage.textContent="";
+  }
+}
--- resources/views/Registration.blade.php
+++ resources/views/Registration.blade.php
@@ -20,26 +20,33 @@
                 <span></span>
                 <label>Name*</label>
             </div>
-            <span class="text-warning" style="">@error('name'){{$message}}@enderror</span>
+            {{-- <p class="text-warning">@error('name'){{$message}}@enderror</p> --}}
+
             <div class="txt_field">
                 <input type="text" name="Email"  value="{{old('Email')}}" >
                 <label>Email / Mobile*</label>
             </div>
-            <span class="text-warning">  @error('Email') {{ $message }}@enderror</span>
+            {{-- <p class="text-warning">  @error('Email') {{ $message }}@enderror</p> --}}
             <div class="txt_field">
-                <input type="password" name="password"  value="{{old('password')}}" >
+                <input type="password" name="password" id="password"  value="{{old('password')}}" >
                 <label>Password*</label>
             </div>
-            <span class="text-warning">@error('password'){{ $message }}@enderror</span>
+            {{-- <p class="text-warning">@error('password'){{ $message }}@enderror</p> --}}
             <div class="txt_field">
-                <input type="password" name="repassword"  value="{{old('repassword')}}" >
+                <input type="password" name="repassword" 
+                
+                id="confirm-password"  value="{{old('repassword')}}" >
                 <label>Re-Password*</label>
             </div>
-            <span class="text-warning" style="margin-top: -15px">  @error('password')  {{ $message }} @enderror</span>
+            {{-- <p class="text-warning" style="margin-top: -15px">  @error('password')  {{ $message }} @enderror</p> --}}
+            <p id="message" style="color:black !important ; "></p>
             <div>
-                <input type="submit" value="SIGN UP">
+                <input type="submit"
+                onclick="checkpassword"
+                value="SIGN UP">
             </div>
 
         </form>
     </div>
+    <script src="{{ asset('js/navbar.js') }}"></script>
 @endsection
