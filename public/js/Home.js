ea370c2ce8f0e0610c3082c649238aac04f45371
Firoz <firoz.m@etv.co.in>
2023-06-28 17:15:59 +0530

    home page content in progress after that only few content remain

--- public/js/Home.js
+++ public/js/Home.js
@@ -1,23 +1,86 @@
-// public/js/home.js
+
 
 $(document).ready(function() {
-    $('.owl-carousel').owlCarousel({
-        items: 1,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        nav: true,
-        navText: [
-            '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
-        ],
-        dots: true,
-        lazyLoad: true, // Enable lazy loading
-        responsive: {
-            0: {
-                mouseDrag: true,
-                touchDrag: true
-            }
+  $('#main-page').owlCarousel({
+    items:1,
+    loop: false, // Enable loop
+    lazyLoad: true, // Enable lazy loading
+    mouseDrag: true, // Disable mouse drag
+    touchDrag: true, // Enable touch drag
+    nav: true, // Show navigation buttons
+    stagePadding:50,
+    margin: 10,
+    autoWidth:true,
+    autoplayHoverPause: false,
+    center:true,
+    // navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'], // Customize navigation icons
+    responsive: {
+      0: {
+        items: 1 // Number of items to show on different screen sizes
+      },
+      768: {
+        items: 1
+      },
+      1024: {
+        items: 1
+      }
+    }
+  });
+
+  $('#another-carousel').owlCarousel({
+    loop: true,
+    lazyLoad: true,
+    mouseDrag: true,
+    touchDrag: true,
+    nav: true,
+    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    responsive: {
+      0: {
+        items: 2
+      },
+      768: {
+        items: 2
+      },
+      1024: {
+        items: 2
+      }
+    }
+  });
+  $('#Before-TV').owlCarousel({
+    loop:true,
+    lazyLoad:true,
+    mouseDrag:true,
+    nav:false,
+    dots:false,
+    navText:['<>','</>'],
+    responsive:{
+        0:{
+            items:2
+        },
+        668:{
+            items:6
+        },
+        1024:{
+            items:6
         }
-    });
+    }
+  })
+  $('#Live-TV').owlCarousel({
+    loop:false,
+    lazyLoad:true,
+    dots:false,
+    items:7,
+    responsive:{
+        0:{
+            items:3
+        },
+        668:{
+            items:7
+        },
+        1024:{
+            items:7
+        }
+    }
+  })
 });
+
--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -1,38 +1,127 @@
- <!-- Include the Font Awesome CSS if not already included -->
-
- <link rel="stylesheet" href="{{ URL::asset('css/owl.css') }}" />
 <!-- Include the LazyLoad plugin script -->
-<script src="{{URL::asset('js/jquery.min.js')}}"></script>
+<script src="{{ URL::asset('js/jquery.min.js') }}"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
+<link rel="stylesheet" href="{{ URL::asset('css/owl.carousel.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
 <script type="text/javascript" src="{{ URL::asset('js/owl.carousel.js') }}"></script>
 
-<link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
-</head>
-    <div class="owl-carousel owl-theme">
-            <div class="item"><h4>1</h4></div>
-            <div class="item"><h4>2</h4></div>
-            <div class="item"><h4>3</h4></div>
-            <div class="item"><h4>4</h4></div>
-            <div class="item"><h4>5</h4></div>
-            <div class="item"><h4>6</h4></div>
-            <div class="item"><h4>7</h4></div>
-            <div class="item"><h4>8</h4></div>
-            <div class="item"><h4>9</h4></div>
-            <div class="item"><h4>10</h4></div>
-            <div class="item"><h4>11</h4></div>
-            <div class="item"><h4>12</h4></div>
-            {{-- <img class="owl-lazy" data-src="{{ asset('image/tst.jpg' ) }}" alt="Slide Image">
-            <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image">
-          <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image"> --}}      
-     </div> 
+<link rel="stylesheet" href="{{ URL::asset('css/owl.theme.default.css') }}">
+<div class="container-fluid
+" style="background: #00021C !important; margin-bottom:40px">
+    <div class="owl-carousel owl-theme" id="main-page">
+        <div class="item"> <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}"
+                data-src-retina="{{ asset('image/tst.jpg') }}" alt=""></div>
+        <div class="item"> <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image"></div>
+        <picture>
+            {{-- <source class="owl-lazy" media="(min-width: 300px)" data-srcset="{{ asset('image/sample.jpg') }}"> --}}
+            <img class="owl-lazy" data-src="https://placehold.it/350x250&text=3-fallback" alt="">
+        </picture>
+        <div class="item"> <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image"></div>
+        <div class="item"> <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image"></div>
+        <div class="item"> <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image"></div>
+        <div class="item"> <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image"></div>
 
 
-<script>
-    $(document).ready(function () {
-        $(".owl-carousel").owlCarousel(
+    </div>
+    {{-- WIN-Exclusive --}}
+    <div class="WIN Exclusives">
+        <a href="" class="section-title">WIN-Exclusive</a>
+        <a href="" class="see-all-link left-margin-episode ">+More</a>
+        <div class="owl-carousel owl-theme" id="another-carousel">
+            <div class="item">
+                <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image">
+            </div>
+            <div class="item">
+                <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image">
+            </div>
+            <!-- Add more carousel items here -->
+
+        </div>
+    </div>
+    {{-- Before TV --}}
+    <div class="Before TV">
        
-        );
-    });
-</script>
-<script src="{{URL:: asset('js/owl.carousel.min.js') }}"></script>  
+            <a href="" class="section-title">Before TV</a>  
+           <a href="" class="see-all-link left-margin-episode ">+More</a>
+        <div class="owl-carousel owl-theme" id='Before-TV'>
+            <div class="item"
+                style="transform: translate3d(0px, 0px, 0px); transition: all 0s ease 0s; width: 2214px;">
+                <img alt="" class="owl-lazy" data-src="{{ asset('image/sample.jpg') }}">
+            </div>
+            <div class="item">
+                <img alt="" class="owl-lazy" data-src="{{ asset('image/sample.jpg') }}">
+            </div>
+            <div class="item">
+                <img alt="" class="owl-lazy" data-src="{{ asset('image/sample.jpg') }}">
+            </div>
+            <div class="item">
+                <img alt="" class="owl-lazy" data-src="{{ asset('image/sample.jpg') }}">
+            </div>
+            <div class="item">
+                <img alt="" class="owl-lazy" data-src="{{ asset('image/sample.jpg') }}">
+            </div>
+            <div class="item">
+                <img alt="" class="owl-lazy" data-src="{{ asset('image/sample.jpg') }}">
+            </div>
+            <div class="item"> <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image"></div>
+            <div class="item"> <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image"></div>
+            <div class="item"> <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image"></div>
+            <div class="item"> <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image"></div>
+            <div class="item"> <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image">
+            </div>
+            <div class="item"> <img class="owl-lazy" data-src="{{ asset('image/tst.jpg') }}" alt="Slide Image">
+            </div>
+        </div>
+    </div>
+    <div class="Live-Tv">
+        <h2>Live TV</h2>
+        <div class="owl-carousel owl-theme" id="Live-TV">
+            <div class="item card-item">
+                <a href="">
+                    <div class="image-holder">
+                        <img src="https://etv-win-image.akamaized.net/etvwin/live/channel/27931/etv-cinema-270x360.jpg"
+                            lazy-img="https://etv-win-image.akamaized.net/etvwin/live/channel/27931/etv-cinema-270x360.jpg"
+                            alt="thumbnails">
+                        <img src='{{ asset('image/crown-small.png') }}' alt="" class="premium-badge">
+                    </div>
+                </a>
+            </div>
+            <div class="item card-item">
+                <a href="">
+                    <div class="image-holder">
+                        <img src="https://etv-win-image.akamaized.net/etvwin/live/channel/27931/etv-cinema-270x360.jpg"
+                            lazy-img="https://etv-win-image.akamaized.net/etvwin/live/channel/27931/etv-cinema-270x360.jpg"
+                            alt="thumbnails">
+                        <img src='{{ asset('image/crown-small.png') }}' alt="" class="premium-badge">
+                    </div>
+                </a>
+            </div>
+            <div class="item card-item">
+                <a href="">
+                    <div class="image-holder">
+                        <img src="https://etv-win-image.akamaized.net/etvwin/live/channel/27931/etv-cinema-270x360.jpg"
+                            lazy-img="https://etv-win-image.akamaized.net/etvwin/live/channel/27931/etv-cinema-270x360.jpg"
+                            alt="thumbnails">
+                        <img src='{{ asset('image/crown-small.png') }}' alt="" class="premium-badge">
+                    </div>
+                </a>
+            </div>
+            <div class="item card-item">
+                <a href="">
+                    <div class="image-holder">
+                        <img src="https://etv-win-image.akamaized.net/etvwin/live/channel/27931/etv-cinema-270x360.jpg"
+                            lazy-img="https://etv-win-image.akamaized.net/etvwin/live/channel/27931/etv-cinema-270x360.jpg"
+                            alt="thumbnails">
+                        <img src='{{ asset('image/crown-small.png') }}' alt="" class="premium-badge">
+                    </div>
+                </a>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
+<script src="{{ asset('js/Home.js') }}"></script>
+
+<script src="{{ URL::asset('js/owl.carousel.min.js') }}"></script>
